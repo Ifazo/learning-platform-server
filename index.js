bc5bcb1351d33345187b2637b3e3bff8da485705
ifazo <ziaulkarimifaz@gmail.com>
2022-10-27 11:18:37 +0600

    details added

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(cors());
 
 const data = require('./data/fakedata.json');
 
 app.get('/', (req, res) => {
+  res.send(data)
+})
+
+app.get('/courses', (req, res) => {
   res.send(data)
 })
 
