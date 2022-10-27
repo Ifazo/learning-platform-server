e71caf88784bdf397acf03c7beac811adbbb3512
ifazo <ziaulkarimifaz@gmail.com>
2022-10-27 09:05:00 +0600

    data added site

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
+
+// app.use(cors());
+
+const data = require('./data/fakedata.json');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(data)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}...`)
 })
