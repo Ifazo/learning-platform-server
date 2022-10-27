ee8fbba2ea986e6991ea88ea9132838d98fa6112
ifazo <ziaulkarimifaz@gmail.com>
2022-10-27 14:33:38 +0600

    category added

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
   res.send(data)
 })
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const course = data.find(course => course.id == id);
+  res.send(course);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}...`)
